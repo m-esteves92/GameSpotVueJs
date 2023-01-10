54b74b1b86b4f4e9809066e89687ab0126751e86
Manuel Esteves <mj.esteves92@gmail.com>
2023-01-10 18:54:43 +0000

    12_finish_notifications

--- src/Store/notifications/index.js
+++ src/Store/notifications/index.js
@@ -1,21 +1,25 @@
-
-
-
 const notificationsModule = {
     namespaced:true,
     state(){
         return {
+            loader:false,
             toastMsg:[false,'','error'] // true.false - message - type
         }
     },
     getters:{
         getToastMsg(state){
             return state.toastMsg;
+        },
+        isLoading(state){
+            return state.loader;
         }
     },
     mutations:{
         setToastMsg(state,payload){
             state.toastMsg = [true,payload.msg,payload.type]
+        },
+        setLoading(state,payload){
+            state.loader = payload
         }
     }
 }
