39e7de8e36a8e34c1d3a032a717f0e39a59e194f
Manuel Esteves <mj.esteves92@gmail.com>
2023-01-11 19:16:29 +0000

    20_add_article_from_elements_part_II

--- src/Tools/schemas.js
+++ src/Tools/schemas.js
@@ -2,12 +2,23 @@ import * as yup from 'yup';
 
 
 const addArticleSchema = {
-    game:yup.string(),
-    title:yup.string(),
-    excerpt:yup.string(),
+    game:yup.string()
+        .required('The game is required'),
+    title:yup.string()
+        .required('The title is required')
+        .min(20,'Make the title bigger')
+        .max(70,'The title is too long'),
+    excerpt:yup.string()
+        .required('The excerpt is required')
+        .min(100,'Make the excerpt bigger')
+        .max(320,'The excerpt is too long'),
     editor:yup.string(),
-    rating:yup.string(),
+    rating:yup.string()
+        .required('The excerpt is required')
+        .notOneOf(['Select a rating'],'You need to select a rating'),
     img:yup.string()
+        .url('Is this a valid url ? come one !!')
+        .required('The excerpt is required')
 }
 
 
--- src/components/User/Dashboard/Admin/add.vue
+++ src/components/User/Dashboard/Admin/add.vue
@@ -18,8 +18,82 @@
             </Field>
          </div>
          
+         <br/>
 
+         <div class="form-group">
+            <Field name="title" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="input"
+                  type="text"
+                  placeholder="Title of the article"
+               />
+            </Field>
+         </div>
 
+  
+         <br/>
+
+         <div class="form-group">
+            <Field name="excerpt" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="textarea"
+                  placeholder="Add an excerpt"
+                  :rows="3"
+               />
+            </Field>
+         </div>
+
+         <br/>
+
+         <div class="form-group">
+            <Field 
+               name="rating" 
+               v-slot="{ field, errors, errorMessage }"
+               value="Select a rating"
+            >
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="select"
+               >
+                  <option value="Select a rating" selected>Select a rating</option>
+                  <option v-for="rating in ratings" :key="rating" :value="rating">
+                     {{ rating }}
+                  </option>
+               </form-element>
+            </Field>
+         </div>
+         
+         <br/>
+
+         <div class="form-group">
+            <Field name="img" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="input"
+                  type="text"
+                  placeholder="Add the source of the img"
+               />
+            </Field>
+         </div>
+
+         <br/>
+
+         <button
+         type="submit"
+         class="btn btn-primary"
+         >
+            Add article
+         </button>
    </Form>
 
 </template>
@@ -30,6 +104,8 @@ import FormElement from '../../../Utils/form_elements.vue';
 import { addArticleSchema } from '@/Tools/schemas.js';
 import DashboardTitle from '../../../Utils/dashTitle.vue';
 import { Field, Form} from 'vee-validate';
+
+
 export default {
    components:{
       DashboardTitle,
@@ -39,6 +115,7 @@ export default {
    },
    data(){
       return {
+         ratings:[1,2,3,4,5],
          formSchema:addArticleSchema
       }
    },
@@ -48,4 +125,5 @@ export default {
       }
    }
 }
+
 </script>
