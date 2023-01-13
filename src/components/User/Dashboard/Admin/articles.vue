e2d5aa30564918a5639ae9cb502272741534a584
Manuel Esteves <mj.esteves92@gmail.com>
2023-01-13 11:24:54 +0000

    24_get_admin_articles

--- src/components/User/Dashboard/Admin/articles.vue
+++ src/components/User/Dashboard/Admin/articles.vue
@@ -1,16 +1,30 @@
 <template>
-    <dashboard-title 
-       title="Articles"
-    />
- </template>
- 
- 
- <script>
- import DashboardTitle from '../../../Utils/dashTitle.vue';
- 
- export default {
-    components:{
-       DashboardTitle
-    }
- }
- </script>
+   <dashboard-title 
+      title="Articles"
+   />
+</template>
+
+
+<script>
+import DashboardTitle from '../../../Utils/dashTitle.vue';
+import { mapActions } from 'vuex'
+
+export default {
+   components:{
+      DashboardTitle
+   },
+   computed:{
+      adminArticles(){
+         return this.$store.getters['articles/getAdminArticles']
+      }
+   },
+   mounted(){
+      this.getAdminArticles({limit:3})
+   },
+   methods:{
+      ...mapActions('articles',[
+         'getAdminArticles'
+      ])
+   }
+}
+</script>
