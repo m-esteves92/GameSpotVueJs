51ca8294426be8ceadbd2bdb2332073576eaf370
Manuel Esteves <mj.esteves92@gmail.com>
2023-01-13 11:38:52 +0000

    25_admin_articles_table

--- src/components/User/Dashboard/Admin/articles.vue
+++ src/components/User/Dashboard/Admin/articles.vue
@@ -2,6 +2,55 @@
    <dashboard-title 
       title="Articles"
    />
+
+   <div v-if="adminArticles">
+
+      <a-table
+         :columns="columns"
+         :pagination="false"
+         :data-source="adminArticles"
+         :row-key="record => record.id"
+         bordered
+      >
+         <template #name="{ text }">
+            <a>{{ text }}</a>
+         </template>
+
+         <template #game="{ record }">
+            <router-link :to="{name:'article',params:{id:record.id}}" target="_blank">
+               {{ record.game }}
+            </router-link>
+         </template>
+
+         <template #owner="{ record }">
+            <span>
+               {{ record.owner.firstname}}  {{ record.owner.lastname}}
+            </span>
+         </template>
+
+         <template #time="{ record }">
+            <span>
+               {{ record.timestamp.toDate().toDateString()}} 
+            </span>
+         </template>
+
+
+         <template #delete="{ record }">
+            <a-popconfirm
+               title="Are you sure ?"
+               ok-text="Yes"
+               cancel-text="No"
+               @confirm="removeById(record.id)"
+            >
+               <button class="btn btn-danger btn-sm">Delete article</button>
+            </a-popconfirm>
+         </template>
+
+
+      </a-table>
+
+   </div>
+
 </template>
 
 
@@ -12,6 +61,11 @@ import { mapActions } from 'vuex'
 export default {
    components:{
       DashboardTitle
+   },
+   data(){
+      return {
+         columns
+      }
    },
    computed:{
       adminArticles(){
@@ -24,7 +78,35 @@ export default {
    methods:{
       ...mapActions('articles',[
          'getAdminArticles'
-      ])
+      ]),
+      removeById(id){
+         console.log(id)
+      }
    }
 }
+
+const columns = [
+   {
+      title:'Game',
+      slots:{ customRender:'game'}
+   },
+   {
+      title:'Owner',
+      slots:{ customRender: 'owner'}
+   },
+   {
+      title:'Rating',
+      dataIndex:'rating',
+   },
+    {
+      title:'Created at',
+      slots:{ customRender: 'time'}
+   },
+   {
+      title:'',
+      slots:{ customRender: 'delete'}
+   }
+]
+
+
 </script>
